c7eb9a31a7dfb5990c7875b701142d2f036d58f2
gilang <itgilangprambudi@gmail.com>
2021-01-20 06:45:57 +0700

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module jenkins_test_project
+
+go 1.15
+
+require github.com/gorilla/mux v1.8.0
---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", index)
+
+	srv := &http.Server{
+		Handler:      r,
+		Addr:         "0.0.0.0:6001",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "Application/json")
+	json.NewEncoder(w).Encode(map[string]bool{"ok": true})
+}
